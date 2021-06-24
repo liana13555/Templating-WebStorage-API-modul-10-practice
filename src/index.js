bf819d617dcd9fc64b7260ab6bbe60e34bb892da
Liana Baryshova <barishovaliana@gmail.com>
2021-06-24 20:06:05 +0200

    Update files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles/main.scss";
+import template from "./templates/main.hbs";
+import data from "./data/data.json";
+import {setLocalStorage, getLocalStorage} from "./utils/utils"
+
+let tmpUserNameState = "";
+
+window.onload = () => {
+    const container = document.getElementById("container");
+
+    container.innerHTML = template(data);
+
+    const nameInput = document.getElementById("nameInput");
+
+    const saveUser = document.getElementById("saveUser");
+
+    nameInput.addEventListener("input", e => {
+        tmpUserNameState = e.target.value;
+    })
+
+    saveUser.addEventListener("click", e => {
+        setLocalStorage(tmpUserNameState);
+        const newData = Object.assign({}, data, { name: tmpUserNameState });
+        container.innerHTML = template(newData);
+    })
+};
+
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,18 @@
-export const test = () => {
-  console.log("lll");
+export const setLocalStorage = (name) => {
+  localStorage.setItem("userName", name);
+
+  try {
+    localStorage.setItem("userName", name);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+export const getLocalStorage = () => {
+  try {
+    const isNameExists = localStorage.getItem("userName");
+    if (!isNameExists) return false;
+    } catch (error) {
+    console.log(error);
+  }
+}
